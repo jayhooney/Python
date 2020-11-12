0b28edf5aa7f870a5300c7dc9e99be15659c1175
jay <hooney731@gmail.com>
2020-11-12 16:08:24 +0900

    셀렉터/옵션 파일 분리 완료

---
+++ HansungCrawler/Define/Config.py
@@ -0,0 +1,5 @@
+class CrawlerConf:
+    CHROME_DRIVER_PATH = "./ChromeDriver/chromedriver_mac64/chromedriver"
+    URL = "https://www.monsterlabs.co.kr/src/category/read.html?pn=44004"
+    # 내가 찾는 옵션 문자열
+    MY_WISH = "화이트 한글키캡"
