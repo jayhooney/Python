a1a096507f8371094ce303810031764cdf909349
jay <dev_jhlee@wisenut.co.kr>
2020-11-12 10:27:05 +0900

    css selector를 적용해서 원하는 node의 innerText 가져오기 성공

--- HansungCrawler/App.py
+++ HansungCrawler/App.py
@@ -1,9 +1,21 @@
 import time
 from selenium import webdriver
 
-driver = webdriver.Chrome("./ChromeDriver/chromedriver_mac64/chromedriver")
 
-driver.get('https://www.monsterlabs.co.kr/src/category/read.html?pn=44004')
+# 크롬 드라이버 세팅
+browser = webdriver.Chrome("./ChromeDriver/chromedriver_mac64/chromedriver")
+
+# 한성컴퓨터 GK898B OfficeMaster 무접점 키보드 (블루투스 5.0) 페이지로 이동.
+browser.get('https://www.monsterlabs.co.kr/src/category/read.html?pn=44004')
+
+# 현재 존재하는 옵션들 크롤링
+elementList = browser.find_elements_by_css_selector(
+    "body > ul > li:nth-child(2) > a")
+
+for element in elementList:
+    print(element.get_attribute("innerText"))
+
+
 time.sleep(10)
 
-driver.quit()
+browser.quit()
---
+++ HansungCrawler/README.md
@@ -0,0 +1,3 @@
+# Python Toy project
+
+#### 한성컴퓨터 GK898B OfficeMaster 무접점 키보드 (블루투스 5.0) 화이트/한글각인 제품 재입고 확인용 크롤러.
