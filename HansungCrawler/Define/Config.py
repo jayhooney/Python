54615c124b5f1bb8676d23919049856d172c569c
jay <hooney731@gmail.com>
2020-11-20 09:44:52 +0900

    한성 키보드 옵션 선택 리스트 셀렉터 수정

--- HansungCrawler/Define/Config.py
+++ HansungCrawler/Define/Config.py
@@ -2,4 +2,4 @@ class CrawlerConf:
     CHROME_DRIVER_PATH = "./ChromeDriver/chromedriver_mac64/chromedriver"
     URL = "https://www.monsterlabs.co.kr/src/category/read.html?pn=44004"
     # 내가 찾는 옵션 문자열
-    MY_WISH = "화이트 한글키캡"
+    MY_WISH = "화이트 한글-영문키캡"
