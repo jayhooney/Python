30aa095932bad6df9143e6cdc370712549cff365
jay <dev_jhlee@wisenut.co.kr>
2020-11-20 22:43:40 +0900

    format 테스트 완료

--- Notion/NotionUtil.py
+++ Notion/NotionUtil.py
@@ -8,27 +8,16 @@ class NotionUtil:
     def getSchema(self):
         return {
             # title 항상 존재 해야한다
-            "title": {"name": "내용", "type": "title"},
-            "complete": {"name": "체크박스", "type": "checkbox"},
-            "priority": {"name": "셀릭트박스", "type": "select",
-                         "options": [
-                             {
-                                 "color": "red",
-                                 "id": "502c7016-ac57-413a-90a6-64afadfb0c44",
-                                 "value": "사과",
-                             },
-                             {
-                                 "color": "yellow",
-                                 "id": "59560dab-c776-43d1-9420-27f4011fcaec",
-                                 "value": "오렌지",
-                             },
-                             {
-                                 "color": "green",
-                                 "id": "57f431ab-aeb2-48c2-9e40-3a630fb86a5b",
-                                 "value": "수박",
-                             }
-                         ]
-                         }
+            "title": {"name": "날짜", "type": "date"},
+            "priority": {"name": "IS_야근", "type": "checkbox"},
+            "complete_1": {"name": "JOB_TITLE_1", "type": "text"},
+            "complete_2": {"name": "JOB_TITLE_2", "type": "text"},
+            "complete_3": {"name": "JOB_TITLE_3", "type": "text"},
+            "complete_4": {"name": "JOB_TITLE_4", "type": "text"},
+            "complete_5": {"name": "JOB_TITLE_5", "type": "text"},
+            "complete_6": {"name": "JOB_TITLE_6", "type": "text"},
+            "complete_7": {"name": "JOB_TITLE_7", "type": "text"},
+
         }
 
     def Start(self):
@@ -40,11 +29,17 @@ class NotionUtil:
             client.create_record(
                 "collection", parent=child, schema=self.getSchema())
         )
-        child.title = '생성한 테이블'
+        child.title = 'TEST TABLE'
         child.views.add_new(view_type="table")
 
         row = child.collection.add_row()
-        row.set_property('title', '추가된 아이템')
-        row.set_property('체크박스', True)
-        row.set_property('셀릭트박스', '사과')
+        row.set_property('날짜', "2020/11/20 9:00-18:00")
+        row.set_property('IS_야근', True)
+        row.set_property('JOB_TITLE_1', 'dev_1')
+        row.set_property('JOB_TITLE_2', 'dev_1')
+        row.set_property('JOB_TITLE_3', 'dev_1')
+        row.set_property('JOB_TITLE_4', 'dev_1')
+        row.set_property('JOB_TITLE_5', 'dev_1')
+        row.set_property('JOB_TITLE_6', 'dev_1')
+        row.set_property('JOB_TITLE_7', 'dev_1')
         return
